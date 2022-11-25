2da6e73cfc5f15c209210a5d905b1a7230747f4d
Justin Berger Howes <howes.j.j@gmail.com>
2022-11-25 12:31:11 +1100

    saveToLocal added as an instance variable on TaskManager class

--- js/classes/TaskManager.js
+++ js/classes/TaskManager.js
@@ -1,13 +1,16 @@
 class TaskManager {
     static id = localStorage.getItem("latestId") > 0 ? localStorage.getItem("latestId") : 0
-    
+    static tasks = []
     constructor(name, description, assignedTo, dueDate, status){
         this.id = TaskManager.id++;
         this._name = name;
         this._description = description;
         this._assignedTo = assignedTo;
         this._dueDate = dueDate;
-        this._status = status
+        this._status = status;
+        const saveToLocal = () => {
+            TaskManager.tasks.push(this)
+        }
     }
 
     get name(){
@@ -29,4 +32,6 @@ class TaskManager {
     static getAllTasks(){
         return JSON.parse(localStorage.getItem("tasks"))
     }
+
+
 }
