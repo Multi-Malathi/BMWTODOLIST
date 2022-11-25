ba788824c7946018e0e1ea2d91b23013657fef5e
Justin Berger Howes <howes.j.j@gmail.com>
2022-11-25 12:28:59 +1100

    getAllTasks added to TaskManager class

--- js/classes/TaskManager.js
+++ js/classes/TaskManager.js
@@ -1,11 +1,13 @@
 class TaskManager {
     static id = localStorage.getItem("latestId") > 0 ? localStorage.getItem("latestId") : 0
-    constructor(name, description, assignedTo, dueDate,){
+    
+    constructor(name, description, assignedTo, dueDate, status){
         this.id = TaskManager.id++;
         this._name = name;
         this._description = description;
         this._assignedTo = assignedTo;
         this._dueDate = dueDate;
+        this._status = status
     }
 
     get name(){
@@ -22,5 +24,9 @@ class TaskManager {
 
     get dueDate(){
         return this._dueDate;
+    }
+
+    static getAllTasks(){
+        return JSON.parse(localStorage.getItem("tasks"))
     }
 }
